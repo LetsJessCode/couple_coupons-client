771f440067b8cec652f8d5a0bf8a72340e61ac83
LetsJessCode <letsjesscode@gmail.com>
2020-10-07 14:58:13 -0400

    initial commit

---
+++ src/components/CouponCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CouponCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CouponCard
---
+++ src/components/CouponForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CouponForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CouponForm
